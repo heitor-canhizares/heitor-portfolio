63d84ea987feaedd7742ea6e5d038153afe80276
Heitor Eduardo Avelar Theodoro Canhizares <44684991+heitor-canhizares@users.noreply.github.com>
2023-06-09 14:35:09 -0400

    Add files via upload

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Footer() {
+    return (
+        <footer className="footer p-10 bg-zinc-800 text-neutral-content justify-center self-end ">
+            <div></div>
+        </footer>
+    );
+}
