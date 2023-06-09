0fa4cff5037c6b0c6f9ab140480e4f6d9261a89c
Heitor Eduardo Avelar Theodoro Canhizares <44684991+heitor-canhizares@users.noreply.github.com>
2023-06-09 14:43:09 -0400

    Delete src directory

--- src/components/footer.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Footer() {
-    return (
-        <footer className="footer p-10 bg-zinc-800 text-neutral-content justify-center self-end ">
-            <div></div>
-        </footer>
-    );
-}
